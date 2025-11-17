3756ec48775a09c0b9053c60701307a1e564d788
soochan <audtncks77@naver.com>
2025-11-17 15:46:50 +0900

    feat(feature-category): 카테고리 상세 TabBar CommonTopBar로 변경

--- feature/category/src/main/java/com/chan/category/ui/composables/detail/CategoryDetailScreen.kt
+++ feature/category/src/main/java/com/chan/category/ui/composables/detail/CategoryDetailScreen.kt
@@ -1,30 +1,19 @@
 package com.chan.category.ui.composables.detail
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.ripple.LocalRippleTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ScrollableTabRow
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.chan.android.NoRippleTheme
 import com.chan.android.model.ProductsModel
 import com.chan.android.ui.CommonProductsCard
-import com.chan.android.ui.theme.Black
-import com.chan.android.ui.theme.Gray
+import com.chan.android.ui.composable.CommonTabBar
 import com.chan.android.ui.theme.Spacing
+import com.chan.android.ui.theme.White
 import com.chan.category.ui.CategoryDetailContract
 import com.chan.category.ui.model.detail.CategoryDetailTabsModel
 
@@ -66,14 +55,14 @@ fun CategoryDetailProductGrid(
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        modifier = modifier,
+        modifier = modifier.background(White),
         contentPadding = PaddingValues(Spacing.spacing2)
     ) {
         items(
             items = products,
             key = { it.productId }
         ) { product ->
-            CommonProductsCard (
+            CommonProductsCard(
                 product = product,
                 onClick = { onEvent(CategoryDetailContract.Event.OnProductClick(it)) },
                 onLikeClick = {},
@@ -91,32 +80,12 @@ fun CategoryDetailTopBar(
     selectedIndex: Int,
     onTabSelected: (String) -> Unit
 ) {
-    ScrollableTabRow(
-        modifier = Modifier.fillMaxWidth(),
+
+    CommonTabBar(
+        tabs = tabs.map { it.categoryName },
         selectedTabIndex = selectedIndex,
-        edgePadding = Spacing.spacing4,
-        indicator = { tabPositions ->
-            Box(
-                Modifier
-                    .tabIndicatorOffset(tabPositions[selectedIndex])
-                    .height(2.dp)
-                    .background(Black)
-            )
-        },
-    ) {
-        CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
-            tabs.forEachIndexed { index, title ->
-                Tab(
-                    selected = selectedIndex == index,
-                    onClick = { onTabSelected(title.categoryId) },
-                    text = {
-                        Text(
-                            text = title.categoryName,
-                            color = if (index == selectedIndex) Black else Gray
-                        )
-                    }
-                )
-            }
+        onTabClick = {
+            onTabSelected(tabs[it].categoryId)
         }
-    }
+    )
 }
